6fd132a0863dba4f59699a80cb4da517c7b4e35e
JVNunes50 <jvnunes1944@gmail.com>
2025-06-12 22:18:33 -0300

    Método Destroy

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -5,6 +5,11 @@
             <li>{{ $c['nome'] }} 
                 <a href="{{ route ('clientes.edit', $c['id']) }}">Editar<a>
                 <a href="{{ route ('clientes.show', $c['id']) }}">Info</a>
+                <form action="{{ route('clientes.destroy', $c['id']) }}" method="post">
+                    @csrf
+                    @method('delete')
+                    <input type="submit" value="Apagar">
+                </form>
             </li>
     @endforeach
 </ol>
