e9436f657fc64da7a30e7e6abefca242c022132e
JVNunes50 <jvnunes1944@gmail.com>
2025-06-15 15:56:42 -0300

    if / else

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -1,15 +1,27 @@
 <h3>{{$titulo}}</h3>
 <a href="{{ route ('clientes.create') }}">Novo cliente</a>
-<ul>
-    @foreach ($clientes as $c)
-            <li>{{ $c['id'] }} {{ $c['nome'] }} 
-                <a href="{{ route ('clientes.edit', $c['id']) }}">Editar<a>
-                <a href="{{ route ('clientes.show', $c['id']) }}">Info</a>
-                <form action="{{ route('clientes.destroy', $c['id']) }}" method="post">
-                    @csrf
-                    @method('delete')
-                    <input type="submit" value="Apagar">
-                </form>
-            </li>
-    @endforeach
-</ul>
+
+@if(count($clientes) > 0)
+
+    <ul>
+        @foreach ($clientes as $c)
+                <li>{{ $c['id'] }} {{ $c['nome'] }} 
+                    <a href="{{ route ('clientes.edit', $c['id']) }}">Editar<a>
+                    <a href="{{ route ('clientes.show', $c['id']) }}">Info</a>
+                    <form action="{{ route('clientes.destroy', $c['id']) }}" method="post">
+                        @csrf
+                        @method('delete')
+                        <input type="submit" value="Apagar">
+                    </form>
+                </li>
+        @endforeach
+    </ul>
+@else
+    <h4>Não existem clientes cadastrados</h4>
+@endif
+
+{{-- Faz a mesma coisa que o ELSE
+@empty($clientes)
+    <h4>Não existem clientes cadastrados</h4>
+@endempty 
+--}} 
