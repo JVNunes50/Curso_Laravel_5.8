a8f76ba9d12d20f7ca847f9cd5633ed044d9f8c3
JVNunes50 <jvnunes1944@gmail.com>
2025-06-15 16:45:50 -0300

    Declarando estilos

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -5,63 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>View</title>
-    <style>
-    .row {
-        display: flex;
-    }
-
-    .col1 {
-        padding: 10px;
-    }
-
-    .col2 {
-        padding: 10px;
-    }
-
-    .menu ul
-    {
-        margin: 0px;
-        padding: 0px;
-        list-style-type: none;
-    }
-
-    .menu li
-    {
-        list-style: none;
-    }
-
-    .menu a
-    {
-        display: block;
-        width: 200px;
-        height: 20px;
-        color: black;
-        background-color: #FFFFFF;
-        text-decoration: none;
-        text-align: center;
-        margin: 5px;
-        font-size: 18px;
-    }
-
-    .menu a:hover
-    {
-        background-color: #b9bcd0;
-        color: #0000ff;
-        font-weight: bold;
-    }
-
-    .menu .active
-    {
-        background-color: #c0d4f3;
-    }
-    </style>
+    <link rel="stylesheet" href="{{ asset('css/principal.css') }}">
 </head>
 <body>
     <div class="row">
         <div class="col1">
             <div class="menu">
                 <ul>
-                    <li><a class="active" href="{{route('clientes.index')}}"></a>Clientes</li>
+                    <li><a class="active" href="{{ route('clientes.index') }}"></a>Clientes</li>
                     <li><a href=""></a>Produtos</li>
                     <li><a href=""></a>Departamento</li>
                 </ul>
